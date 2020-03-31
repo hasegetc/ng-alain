8c29d33ab339e4d90f877c9cbdffdfe2e200df2c
liuyuchao <hubeiliuyuchao@gmail.com>
2020-03-31 14:15:08 +0800

    add command test

--- commands/lyc-command.groovy
+++ commands/lyc-command.groovy
@@ -9,7 +9,29 @@ description( "TODO: command description" ) {
 }
 
 if(args) {
-    // TODO: implement command
+    def modelName = args[0]
+
+    def command = "pwd"
+    def execute = command.execute()
+    println execute.getText()
+
+    def createModuleCommand = "ng g ng-alain:module ${modelName}"
+    execute = createModuleCommand.execute(null, new File("../client"))
+    execute.waitFor()
+
+
+    createModuleCommand = "ng g ng-alain:curd curd -m ${modelName}"
+    execute.waitFor()
+    println createModuleCommand
+
+    execute = createModuleCommand.execute(null, new File("../client"))
+
+    def model = model('/Users/lyc/SourceCode/test/grails-alain-test/server/grails-app/domain/xyz/lyc/Address')
+//    println model.asMap().toString()
+
+    println ""
+    println model.getPropertyName()
+
     println args.toString()
     println "liuyuchao"
 }
